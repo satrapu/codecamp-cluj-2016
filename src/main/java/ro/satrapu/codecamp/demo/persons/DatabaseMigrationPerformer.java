3172ae96661c61ab3bf7e81b7dab0635884ded4b
satrapu <bogdan.marian77@gmail.com>
2016-11-07 23:35:12 +0200

    Renamed class to better match its purpose.

---
+++ src/main/java/ro/satrapu/codecamp/demo/persons/DatabaseMigrationPerformer.java
@@ -0,0 +1,69 @@
+package ro.satrapu.codecamp.demo.persons;
+
+import com.googlecode.flyway.core.Flyway;
+import com.googlecode.flyway.core.api.MigrationInfo;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+import javax.ejb.TransactionManagement;
+import javax.ejb.TransactionManagementType;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * Performs database migration via <a href="https://flywaydb.org/">Flyway</a>.
+ * In order to avoid <a href="https://github.com/flyway/flyway/issues/324">this Flyway issue</a>, this bean
+ * is managing the database transactions.
+ */
+@Startup
+@Singleton
+@TransactionManagement(TransactionManagementType.BEAN)
+public class DatabaseMigrationPerformer {
+    private static final String APPLICATION_CONFIG_FILE_NAME = "app.properties";
+    private static final String PROPERTY_DATASOURCE_NAME = "datasource.name";
+
+    @PostConstruct
+    public void initialize() throws IOException, NamingException {
+        //get the DataSource instance pointing to the underlying database
+        Properties applicationProperties = getApplicationProperties();
+        String dataSourceName = getDataSourceName(applicationProperties);
+        Context context = new InitialContext();
+        DataSource dataSource = getDataSource(context, dataSourceName);
+
+        // run database migration scripts
+        Flyway flyway = new Flyway();
+        flyway.setInitOnMigrate(true);
+        flyway.setDataSource(dataSource);
+
+        for (MigrationInfo migrationInfo : flyway.info().all()) {
+            System.out.println(String.format("Migrate task -> version: %s, description: %s, script: %s",
+                    migrationInfo.getVersion(), migrationInfo.getDescription(), migrationInfo.getScript()));
+        }
+
+        flyway.migrate();
+    }
+
+    private Properties getApplicationProperties() throws IOException {
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+
+        try (InputStream inputStream = classLoader.getResourceAsStream(APPLICATION_CONFIG_FILE_NAME)) {
+            Properties applicationProperties = new Properties();
+            applicationProperties.load(inputStream);
+            return applicationProperties;
+        }
+    }
+
+    private String getDataSourceName(Properties properties) {
+        return (String) properties.get(PROPERTY_DATASOURCE_NAME);
+    }
+
+    private DataSource getDataSource(Context context, String dataSourceName) throws NamingException {
+        return (DataSource) context.lookup(dataSourceName);
+    }
+}
