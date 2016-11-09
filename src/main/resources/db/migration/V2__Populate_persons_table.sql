6e6f681d54022f84ad98bf71538c135f517aae3f
Bogdan Marian <bogdan.marian77@gmail.com>
2016-11-09 11:42:47 +0200

    Changed table and column name case to match the one used by the underlying JPA provider.

--- src/main/resources/db/migration/V2__Populate_persons_table.sql
+++ src/main/resources/db/migration/V2__Populate_persons_table.sql
@@ -1,4 +1,4 @@
-INSERT INTO Persons(id, firstname, lastname) VALUES
+INSERT INTO persons(id, firstname, lastname) VALUES
     (1, 'John', 'Doe'),
     (2, 'Vincent', 'Grimaldi'),
     (3, 'Earl', 'Gray')
